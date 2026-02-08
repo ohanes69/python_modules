7b35ea63748e6017ceb6a826b563132c35109fc9
Sacha Markarian <samarkar@z2r10p5.42lyon.fr>
2026-02-08 18:14:58 +0100

    rectification

--- module00/ft_harvest_total.py
+++ module00/ft_harvest_total.py
@@ -1,4 +1,6 @@
 def ft_harvest_total():
-    res = int(input("Day 1 harvest: ")) + int(input("Day 2 harvest: "))
-    + int(input("Day 3 harvest: "))
-    print("Total harvest:", res)
+    day1 = int(input("Day 1 harvest: "))
+    day2 = int(input("Day 2 harvest: "))
+    day3 = int(input("Day 3 harvest: "))
+
+    print("Total harvest:", day1 + day2 + day3)
