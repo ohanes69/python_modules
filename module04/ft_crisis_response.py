44abdf7641cb4d0a37d5dd4aab18705c0d76823f
Sacha Markarian <samarkar@z2r12p4.42lyon.fr>
2026-02-23 19:19:56 +0100

    modif last

--- module04/ft_crisis_response.py
+++ module04/ft_crisis_response.py
@@ -6,7 +6,7 @@ if __name__ == '__main__':
             print(f.read())
     except FileNotFoundError:
         print(
-            "CRISIS ALERT: Attempting access to'lost_archive.txt'...\n"
+            "CRISIS ALERT: Attempting access to 'lost_archive.txt'...\n"
             "RESPONSE: Archive not found in storage matrix\n"
             "STATUS: Crisis handled, system stable\n"
         )
@@ -16,16 +16,20 @@ if __name__ == '__main__':
             print(f.read())
     except PermissionError:
         print(
-            "CRISIS ALERT: Attempting access to'classified_vault.txt'...\n"
+            "CRISIS ALERT: Attempting access to 'classified_vault.txt'...\n"
             "RESPONSE: Security protocols deny access\n"
             "STATUS: Crisis handled, security maintained\n"
         )
 
-    with open('standard_archive.txt', 'r') as f:
-        print(
-            "ROUTINE ACCESS: Attempting access to'standard_archive.txt'..."
-            f"\nSUCCESS: Archive recovered - ``{f.read()}''\n"
-            "STATUS: Normal operations resumed\n"
-        )
+    try:
+        with open('standard_archive.txt', 'r') as f:
+            print(
+                "ROUTINE ACCESS: Attempting access to "
+                "'standard_archive.txt'...\n"
+                f"SUCCESS: Archive recovered - ``{f.read()}''\n"
+                "STATUS: Normal operations resumed\n"
+            )
+    except Exception as err:
+        print(f"Unexpected error occurred: {err}")
 
     print('All crisis scenarios handled successfully. Archives secure.')
