f010d5667ed6f55fcc0c521107ec44a47352effa
Sacha Markarian <samarkar@z4r4p6.42lyon.fr>
2026-03-11 16:44:55 +0100

    use 2 more functions

--- module07/ex4/TournamentCard.py
+++ module07/ex4/TournamentCard.py
@@ -5,7 +5,7 @@ from ex4.Rankable import RankableClass
 
 class TournamentCardClass(CardClass, CombatableClass, RankableClass):
     def __init__(
-            self, name: str, cost: int, rarity: str,  card_id: str,
+            self, name: str, cost: int, rarity: str, card_id: str,
             rating: int) -> None:
         super().__init__(name, cost, rarity)
         self.wins: int = 0
@@ -13,29 +13,32 @@ class TournamentCardClass(CardClass, CombatableClass, RankableClass):
         self.rating = rating
         self.card_id = card_id
 
-    def play(self, game_state: dict) -> dict:
+    def __repr__(self):
+        return (
+            f"{self.name} - Rating: {self.rating} ({self.wins}-{self.losses})"
+        )
 
+    def play(self, game_state: dict) -> dict:
         game_state = {
             "played": self.name
         }
         return (game_state)
 
     def attack(self, target) -> dict:
-
         won = self.rating > target.rating
 
         if won:
             self.rating += 16
             target.rating -= 16
-            self.wins += 1
-            target.losses += 1
+            self.update_wins(1)
+            target.update_losses(1)
             winner = self.card_id
             loser = target.card_id
         else:
             target.rating += 16
             self.rating -= 16
-            target.wins += 1
-            self.losses += 1
+            target.update_wins(1)
+            self.update_losses(1)
             winner = target.card_id
             loser = self.card_id
 
@@ -52,24 +55,27 @@ class TournamentCardClass(CardClass, CombatableClass, RankableClass):
         return (self.rating)
 
     def get_tournament_stats(self) -> dict:
+        return self.get_rank_info()
+
+    def defend(self, incoming_damage: int) -> dict:
+        return {}
+
+    def get_combat_stats(self) -> dict:
+        return {
+            'attack_result': self.attack(self),
+            'defend_result': self.defend(0)
+        }
+
+    def update_wins(self, wins: int) -> None:
+        self.wins += wins
+
+    def update_losses(self, losses: int) -> None:
+        self.losses += losses
+
+    def get_rank_info(self) -> dict:
         return {
             'player': self.name,
             'rating': self.rating,
             'wins': self.wins,
             'losses': self.losses,
         }
-
-    def defend(self, incoming_damage: int) -> dict:
-        pass
-
-    def get_combat_stats(self) -> dict:
-        pass
-
-    def update_wins(self, wins: int) -> None:
-        pass
-
-    def update_losses(self, losses: int) -> None:
-        pass
-
-    def get_rank_info(self) -> dict:
-        pass
